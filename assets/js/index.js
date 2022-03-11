b257e0267d761a4d184bdf217636c725ac7e22cb
MaxAvila <maxvilam@hotmail.con>
2022-03-11 02:28:32 -0300

    ok async

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,22 +4,32 @@ import Oso from "./Oso.js";
 import Serpiente from "./Serpiente.js";
 import Animal from "./Animal.js";
 import Aguila from "./Aguila.js";
-const dataAnimal = "";
 
 const animal = document.getElementById("animal");
 const preview = document.getElementById("preview");
 const btnRegistrar = document.getElementById("btnRegistrar");
 const edad = document.getElementById("edad");
 const comentarios = document.getElementById("comentarios");
-const cardanimal = document.getElementById("cardanimal");
-
-const aguila1 = new Aguila(
-  "Cuello Blanco",
-  10,
-  "Aguila.png",
-  "nada especial",
-  "Chillido.mp3"
-);
+const obtenerData = (async () => {
+  try {
+    const URL = "animales.json";
+    const request = await fetch(URL);
+    const data = await request.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+})();
+/* const dataAnimal = (async (animal) => {
+  const data =await obtenerData;
+  const dataA =data.animales;
+  for (const iterator of dataA) {
+    if (iterator.name==animal) {
+      console.log("Iterador " +iterator.imagen);
+      return iterator.imagen;
+    }    
+  };
+})(); */
 
 btnRegistrar.addEventListener(
   "click",
@@ -38,6 +48,26 @@ btnRegistrar.addEventListener(
         }
       }
     }
+    
+    const dataAnimal = (async () => {
+      const data = await obtenerData;
+      const dataA = data.animales;
+      for (const iterator of dataA) {
+        if (iterator.name == animal.value) {
+          console.log(iterator.sonido + "Iterador " + iterator.imagen);
+          const ruta = "./assets/imgs/" + iterator.imagen;
+          const animalcard1 = new Animal(
+            animal.value,
+            edad.value,
+            ruta,
+            comentarios.value,
+            iterator.sonido
+          );
+          animalcard1.listAnimals("cardanimal");
+          return;
+        }
+      }
+    })();
     let ruta = "";
     switch (animal.value) {
       case "Leon":
@@ -56,20 +86,12 @@ btnRegistrar.addEventListener(
         ruta = "Aguila.png";
         break;
     }
-    ruta = "./assets/imgs/" + ruta;
-    const animalcard1 = new Animal(
-      animal.value,
-      edad.value,
-      ruta,
-      comentarios.value,
-      "Aullido"
-    );
-    animalcard1.listAnimals("cardanimal");
   },
   false
 );
-(function limpia() {preview.innerHTML =""})();
-
+(function limpia() {
+  preview.innerHTML = "";
+})();
 
 animal.addEventListener("change", function () {
   const imganimal = animal.value;
